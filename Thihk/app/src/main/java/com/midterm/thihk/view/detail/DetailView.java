0823985bf1e5aa4bf794879ca24fff1f54a108db
hieuneko <hieuneko@gmail.com>
2021-10-24 19:42:59 +0700

    1st

---
+++ Thihk/app/src/main/java/com/midterm/thihk/view/detail/DetailView.java
@@ -0,0 +1,5 @@
+package com.midterm.thihk.view.detail;
+import com.midterm.thihk.model.Plants;
+public interface DetailView {
+    //TODO #4 Add void method  for showLoading, hideLoading, setMeal, onErrorLoading;
+}
